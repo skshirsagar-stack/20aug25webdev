7120177641409e54f3432581caedb42e8672c525
Your Name <youremail@gmail.com>
2025-12-28 22:30:20 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-console.log("Hello from index.js");
+  import {f1} from  './index2.js';
+//const {data,data1,f1} = require('./index2.js');
+//const f1 = require('./index2.js');
+
+console.log("hello world :- in external" ,f1());
---
+++ js/index2.js
@@ -0,0 +1,8 @@
+let data = "sagar";
+let data1 = 1;
+const f1 = () => {
+   return "hello";
+};
+
+module.exports = { f1 };
+ 
